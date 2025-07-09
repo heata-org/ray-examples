7b9270ba169d2ce579de0759c0030606e91946da
Joshua O'Connor <joshua@heata.co>
2025-07-09 13:44:03 +0100

    add licence

--- xgboost/xgboost-example.py
+++ xgboost/xgboost-example.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+
+# Copyright 2023 Ray Authors
+# Copyright 2025 heata <tech@heata.co> - Tweaked dataset and made configurable
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Tweaked from https://github.com/ray-project/ray/blob/3cff3e55e5aaa87cca0be5f7be720fc926deede7/doc/source/tune/examples/tune-xgboost.ipynb
+
+
 import sklearn.datasets
 import sklearn.metrics
 from sklearn.model_selection import train_test_split

a354542a43a1d7f2fb9ab767a2352feb92beb542
Joshua O'Connor <joshua@heata.co>
2025-07-09 11:50:49 +0100

    move to synthetic dataset to make intensity easier to manage

--- xgboost/xgboost-example.py
+++ xgboost/xgboost-example.py
@@ -8,7 +8,12 @@ import ray
 
 def train_breast_cancer(config):
     # Load dataset
-    data, labels = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    data, labels = sklearn.datasets.make_classification(
+        n_samples=500 * DATASET_MULTIPLIER,
+        n_features=200,
+        n_informative=50,
+        random_state=0
+    )
     # Split into train and test set
     train_x, test_x, train_y, test_y = train_test_split(data, labels, test_size=0.25)
     # Build input matrices for XGBoost
@@ -40,9 +45,19 @@ config = {
 ray.init()
 
 
+# Alter this to tweak how many samples to measure, this will change the number of Ray actors created by this job
+NUM_SAMPLES = 30
+# Altering this tweaks how intensive the work each Ray actor performs is, this is for simulating a more/less intensive model.
+DATASET_MULTIPLIER = 500
+
+print("Optimizing hyperparameters across heata")
+print(f"NUM_SAMPLES  = {NUM_SAMPLES}   ;    DATASET_MULTIPLIER = {DATASET_MULTIPLIER}")
+
+
+
 tuner = tune.Tuner(
     train_breast_cancer,
-    tune_config=tune.TuneConfig(num_samples=10),
+    tune_config=tune.TuneConfig(num_samples=NUM_SAMPLES),
     param_space=config,
 )
 results = tuner.fit()
